5e5a46cad818df3f24ea195907f6cd853335c866
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-27 16:11:57 -0500

    M&M

---
+++ src/main/java/com/example/mmrelations/MmRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmRelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmRelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mmrelations/MmRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmRelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
